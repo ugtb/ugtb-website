4449bcd03ec3c8c1eceef38750c540d68c6645b6
Oleksandr Sereda <sereda.san@gmail.com>
2023-05-04 20:50:24 +0300

    init: create astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), tailwind()],
+});
