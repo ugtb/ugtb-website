a96c7d025d7b0f89781dddbe58eaa943247ec50b
Marina Sereda <marina.gorb@gmail.com>
2023-05-17 23:48:19 +0300

    refactor: number component

--- src/components/common/Counter.tsx
+++ src/components/common/Counter.tsx
@@ -7,15 +7,17 @@ export interface CounterProps {
 
 export default function Counter({ label, value }: CounterProps) {
   const valueRef = useRef<HTMLParagraphElement>(null);
-
+  const displayValue =
+    value >= 1000 ? Math.floor((value / 1000) * 10) / 10 : value;
   useEffect(() => {
     const initCountUp = async () => {
       const countUpModule = await import('countup.js');
       new countUpModule.CountUp(
         valueRef.current as HTMLParagraphElement,
-        value,
+        displayValue,
         {
           startVal,
+          decimalPlaces: value >= 1000 ? 1 : 0,
           suffix,
           enableScrollSpy: true,
           scrollSpyOnce: true,
@@ -25,13 +27,20 @@ export default function Counter({ label, value }: CounterProps) {
     initCountUp();
   }, []);
 
-  const startVal = Math.floor(value / 2);
-  const suffix = `${value % 10 ? '' : '+'}`;
+  let suffix = '';
+  if (value >= 1000) {
+    suffix = 'k+';
+  }
+  if (!(value % 10) && value < 1000) {
+    suffix = '+';
+  }
+  const startVal =
+    Math.floor(displayValue / 2) < 1 ? 1 : Math.floor(displayValue / 2);
 
   return (
-    <div id="numbers" class="flex w-min flex-col justify-between">
-      <p class="text-md-700 text-brand">{label}</p>
-      <p ref={valueRef} class="text-H2-mobile lg:text-H2-desktop">
+    <div id="numbers" class="flex w-min flex-col justify-between gap-1">
+      <p class=" text-md-700 text-brand">{label}</p>
+      <p ref={valueRef} class=" text-H2-mobile lg:text-H2-desktop">
         {startVal + suffix}
       </p>
     </div>
